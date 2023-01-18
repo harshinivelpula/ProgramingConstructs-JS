b5d94056019a6b2719257a7cac13374e1d4a8536
DELL <105187636+harshinivelpula@users.noreply.github.com>
2023-01-18 16:45:16 +0530

    [charu] selection practice problems with if else if and else

---
+++ SelectionIfElseIf&Else/DisplayWeek.js
@@ -0,0 +1,9 @@
+//Read a Number and Display the week day (Sunday, Monday,...)
+var number = ("3");//enter the number between 1 to 7
+if (number == 1) {console.log("Monday");} 
+else if (number == 2){console.log("Tuesday");}
+else if (number == 3) {console.log("Wednesday");} 
+else if (number == 4){console.log("Thursday");}
+else if (number == 5) {console.log("Friday");} 
+else if (number == 6){console.log("Saturday");}
+else{console.log("Sunday");} 
---
+++ SelectionIfElseIf&Else/NumberIntoDigits.js
@@ -0,0 +1,11 @@
+//Read a Number 1, 10, 100, 1000, etc and display unit, ten, hundred,...
+var number = ("10000000");//enter number between 1 to 10000000
+if (number == 1) {console.log("One");} 
+else if (number == 10){console.log("Ten");}
+else if (number == 100) {console.log("Hundred");} 
+else if (number == 1000){console.log("Thousand");}
+else if (number == 10000) {console.log("Ten Thousand");} 
+else if (number == 100000){console.log("One Lakh");}
+else if (number == 1000000){console.log("Ten Lakhs");}
+else if (number == 10000000){console.log("One Crore");}
+else{console.log("Invalid input");}
